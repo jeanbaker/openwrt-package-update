cf2d4a082b2f5890c72947308de4fddebf2c8073
jeanbaker <148404064+jeanbaker@users.noreply.github.com>
2023-10-19 14:18:53 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -2,8 +2,8 @@
 svn co https://github.com/kenzok8/openwrt-packages/trunk/adguardhome
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome 
 svn co https://github.com/jerrykuku/luci-theme-argon/branches/master ./luci-theme-argon
-svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk 
+svn co https://github.com/xiaorouji/openwrt-passwall-packages
+svn co https://github.com/xiaorouji/openwrt-passwall
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-netdata
 svn co https://github.com/chenmozhijin/luci-app-socat/trunk/luci-app-socat
 
