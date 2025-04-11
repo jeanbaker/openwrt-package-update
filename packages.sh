27b4ba9c1918ee1c0f050c9338a2080b17e78330
jeanbaker <148404064+jeanbaker@users.noreply.github.com>
2025-04-11 08:49:15 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome
-svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome 
+svn co https://github.com/kiddin9/kwrt-packages/trunk/adguardhome
+svn co https://github.com/kiddin9/kwrt-packages/trunk/luci-app-adguardhome 
 svn co https://github.com/xiaorouji/openwrt-passwall-packages/trunk ./
 rm -rf .svn
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk ./
-svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-netdata
-svn co https://github.com/kiddin9/openwrt-packages/trunk/r8125
-svn co https://github.com/kiddin9/openwrt-packages/trunk/r8168
-svn co https://github.com/chenmozhijin/luci-app-socat/trunk/luci-app-socat
+svn co https://github.com/kiddin9/kwrt-packages/trunk/luci-app-netdata
+svn co https://github.com/kiddin9/kwrt-packages/trunk/r8125
+svn co https://github.com/kiddin9/kwrt-packages/trunk/r8168
+git clone https://github.com/chenmozhijin/luci-app-socat ./luci-app-socat
 git clone https://github.com/jerrykuku/luci-theme-argon ./luci-theme-argon
 
 rm -rf .svn
